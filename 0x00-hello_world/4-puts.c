481e0cd578500e806b23a88bf57bfd23588a21bd
AsceTME <veronsmall2002@gmail.com>
2022-06-02 12:33:20 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,38 +1,19 @@
-#!/bin/bash
-#include <stdio.h>                                                                           
+#include <stdio.h>
 
->                                                                                                                                       
+/**
+ *
+ *  * Description: First Point
+ *
+ *   * Return: Always
+ *
+ *    */
 
-> /**                                                                                                                                   
+int main(void)
 
->                                                                                                                                       
+{
 
->  * Description: main - Prints out a string to stdout.                                                                                 
+	puts("\"Programming is like building a multilingual puzzle");
 
->                                                                                                                                       
+	return (0);
 
->  * Return: 0 if success.                                                                                                              
-
->                                                                                                                                       
-
->  */                                                                                                                                   
-
->                                                                                                                                       
-
-> int main(void)                                                                                                                        
-
->                                                                                                                                       
-
-> {                                                                                                                                     
-
->                                                                                                                                       
-
-> puts("\"Programming is like building a multilingual puzzle");                                                                         
-
->                                                                                                                                       
-
-> return (0);                                                                                                                           
-
->                                                                                                                                       
-
-> }
+}	
