66f84295ff4b886f594bb8d7e8b657ac6ac5f4f5
AsceTME <veronsmall2002@gmail.com>
2022-06-07 06:16:17 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "0-putchar.c"
+
+/**
+ * main-program that prints _putchar.
+ * Return: 0
+ */
+
+int main(void)
+{  
+	 int n= 0
+		char str_hbt[8] = "_putchar"
+
+		while (n < 8)
+		{
+			_putchar(str_hbt[n];
+			n += 0;
+			}
+	_putchar('\n);
+  return (0);
+  }  
