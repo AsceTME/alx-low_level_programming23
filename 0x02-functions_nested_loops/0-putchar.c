18b6b55be3cc16faa640fdd45a2608e72537c3f2
AsceTME <veronsmall2002@gmail.com>
2022-06-07 06:29:04 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,41 @@
-#include "0-putchar.c"
+#include "holberton.h"
+
+
 
 /**
- * main-program that prints _putchar.
- * Return: 0
- */
+ *
+ *  * main - program that prints _putchar
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
 
 int main(void)
-{  
-	 int n= 0
-		char str_hbt[8] = "_putchar"
 
-		while (n < 8)
-		{
-			_putchar(str_hbt[n];
-			n += 0;
-			}
-	_putchar('\n);
-  return (0);
-  }  
+{
+
+		int n = 0;
+
+			char str_hbt[8] = "_putchar";
+
+
+
+				while (n < 8)
+
+						{
+
+									_putchar(str_hbt[n]);
+
+											n += 1;
+
+												}
+
+					_putchar('\n');
+
+						return (0);
+
+}
