0fdd79a942fc92c350fc36f2c825c8ba94da277a
AsceTME <veronsmall2002@gmail.com>
2022-06-08 13:07:56 -0700

    a

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,8 +3,11 @@
 
 
 /**
+ *
  *  * main - tests function that prints if integer is positive or negative
+ *
  *   * Return: 0
+ *
  *    */
 
 
@@ -12,10 +15,17 @@
 int main(void)
 
 {
+
 		int i;
+
+
+
 			i = 0;
 
+
+
 				positive_or_negative(i);
+
 					return (0);
 
 }
